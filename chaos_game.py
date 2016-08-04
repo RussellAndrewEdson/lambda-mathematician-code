69078b629895cf83230a21b1f9dbef13d019ee8b
Russell Andrew Edson <russell.andrew.edson@gmail.com>
2016-08-04 18:32:38 +0930

    Modified offsets for the 'MATH' fractal drawer

--- chaos_game.py
+++ chaos_game.py
@@ -56,12 +56,12 @@ def chaos_game(rules, init_point, iterations):
     
     # Plot the points.
     figure = matplotlib.pyplot.figure()
-    #axes = matplotlib.pyplot.axes(xlim=(0, 1), ylim=(0, math.sqrt(3)/2))  
+    axes = matplotlib.pyplot.axes(xlim=(0, 1), ylim=(0, math.sqrt(3)/2))  
     #axes = matplotlib.pyplot.axes(xlim=(0.5, 2.5), ylim=(0, math.sqrt(3)))
     #axes = matplotlib.pyplot.axes(xlim=(-3, 3), ylim=(0, 10))
     #axes = matplotlib.pyplot.axes(xlim=(-2.5, 2.5), ylim=(-2, 2))
     #axes = matplotlib.pyplot.axes(xlim=(0, 1), ylim=(0, 1))
-    axes = matplotlib.pyplot.axes(xlim=(0, 4.6), ylim=(0, 1))
+    #axes = matplotlib.pyplot.axes(xlim=(0, 4.6), ylim=(0, 1))
     matplotlib.pyplot.xlabel("x")
     matplotlib.pyplot.ylabel("y")
     scatterplot = axes.scatter([], [], c="black", edgecolors="none", s=5)
@@ -84,7 +84,7 @@ def chaos_game(rules, init_point, iterations):
                               interval=1, repeat=False, blit=True)
 
     # LibX264 codec for video compression. 
-    animation.save("chaos_game_math.mp4",
+    animation.save("chaos_game_triangle.mp4",
                    extra_args=["-vcodec", "libx264"])
     matplotlib.pyplot.show()
 
@@ -141,16 +141,16 @@ def make_flower_rules():
 def make_fern_rules():
     """The Barnsley fern."""
     rule1 = UpdateRule(0.01, lambda p: (0, 0.16*p[1]),
-                       colors[0])
+                       colors[1])
     rule2 = UpdateRule(0.85, lambda p: (0.85*p[0] + 0.04*p[1],
                                         -0.04*p[0] + 0.85*p[1] + 1.6),
                        colors[1])
     rule3 = UpdateRule(0.07, lambda p: (0.20*p[0] - 0.26*p[1],
                                         0.23*p[0] + 0.22*p[1] + 1.6),
-                       colors[2])
+                       colors[1])
     rule4 = UpdateRule(0.07, lambda p: (-0.15*p[0] + 0.28*p[1],
                                         0.26*p[0] + 0.24*p[1] + 0.44),
-                       colors[3])
+                       colors[1])
     return (rule1, rule2, rule3, rule4)
 
 def make_dragon_rules():
@@ -160,7 +160,7 @@ def make_dragon_rules():
                        colors[0])
     rule2 = UpdateRule(1/2, lambda p: (1/2*p[0] - 1/2*p[1] - 1,
                                        1/2*p[0] + 1/2*p[1]),
-                       colors[1])
+                       colors[2])
     return (rule1, rule2)
 
 def make_carpet_rules():
@@ -203,7 +203,7 @@ def make_math_rules():
                                         + 0.5,
                                         1/9.2*math.sin(math.pi/4)*p[0]
                                         + 1/9.2*math.cos(math.pi/4)*p[1]
-                                        + 0.5),
+                                        + 0.53),
                        colors[2])
     rule4 = UpdateRule(1/12, lambda p: (1/4.6*math.cos(math.pi/2)*p[0]
                                         - 1/4.6*math.sin(math.pi/2)*p[1]
@@ -213,27 +213,27 @@ def make_math_rules():
                        colors[3])
     rule5 = UpdateRule(1/12, lambda p: (1/4.6*math.cos(math.pi/3)*p[0]
                                         - 1/4.6*math.sin(math.pi/3)*p[1]
-                                        + 1.4,
+                                        + 1.3,
                                         1/4.6*math.sin(math.pi/3)*p[0]
                                         + 1/4.6*math.cos(math.pi/3)*p[1]),
                        colors[4])
     rule6 = UpdateRule(1/12, lambda p: (1/9.2*math.cos(0)*p[0]
                                         - 1/9.2*math.sin(0)*p[1]
-                                        + 1.6,
+                                        + 1.57,
                                         1/9.2*math.sin(0)*p[0]
                                         + 1/9.2*math.cos(0)*p[1]
                                         + 0.37),
                        colors[5])
     rule7 = UpdateRule(1/12, lambda p: (1/4.6*math.cos(-math.pi/3)*p[0]
                                         - 1/4.6*math.sin(-math.pi/3)*p[1]
-                                        + 1.9,
+                                        + 1.8,
                                         1/4.6*math.sin(-math.pi/3)*p[0]
                                         + 1/4.6*math.cos(-math.pi/3)*p[1]
-                                        + 0.8),
+                                        + 0.83),
                        colors[6])
     rule8 = UpdateRule(1/12, lambda p: (1/4.6*math.cos(0)*p[0]
                                         - 1/4.6*math.sin(0)*p[1]
-                                        + 2.4,
+                                        + 2.43,
                                         1/4.6*math.sin(0)*p[0]
                                         + 1/4.6*math.cos(0)*p[1]
                                         + 0.75),
@@ -252,10 +252,10 @@ def make_math_rules():
                         colors[9])
     rule11 = UpdateRule(1/12, lambda p: (1/9.2*math.cos(0)*p[0]
                                          - 1/9.2*math.sin(0)*p[1]
-                                         + 3.8,
+                                         + 3.77,
                                          1/9.2*math.sin(0)*p[0]
                                          + 1/9.2*math.cos(0)*p[1]
-                                         + 0.5),
+                                         + 0.45),
                         colors[10])
     rule12 = UpdateRule(1/12, lambda p: (1/4.6*math.cos(math.pi/2)*p[0]
                                          - 1/4.6*math.sin(math.pi/2)*p[1]
@@ -266,9 +266,9 @@ def make_math_rules():
     return (rule1, rule2, rule3, rule4, rule5, rule6,
             rule7, rule8, rule9, rule10, rule11, rule12)
 
-#chaos_game(make_triangle_rules(), (0.1, 0.1), 40000)
+chaos_game(make_triangle_rules(), (0.1, 0.1), 40000)
 #chaos_game(make_flower_rules(), (0.1, 0.1), 40000)
 #chaos_game(make_fern_rules(), (0.1, 0.1), 40000)
 #chaos_game(make_dragon_rules(), (0.1, 0.1), 40000)
 #chaos_game(make_carpet_rules(), (0.1, 0.1), 40000)
-chaos_game(make_math_rules(), (0.1, 0.1), 40000)
+#chaos_game(make_math_rules(), (0.1, 0.1), 40000)
